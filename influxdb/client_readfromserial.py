fd914a0bf10e96165d50d2072faebf05d18fb50e
vara <p.appavoo@gmail.com>
2017-08-20 18:31:07 +0800

    minor changes

--- influxdb/client_readfromserial.py
+++ influxdb/client_readfromserial.py
@@ -5,7 +5,8 @@ import sys
 from time import sleep
 import traceback
 
-port = sys.argv[1]
+nodeid = sys.argv[1]
+port = sys.argv[2]
 ser = serial.Serial(port, 115200)
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect(("localhost", 9000))
@@ -13,11 +14,8 @@ sock.connect(("localhost", 9000))
 while True:
 	try:
 		data = ser.readline()
-		print(data)
-		# for i in range(100):
-		sock.send(data)# + str.encode("\n")) # encode to from str to byte
-			# sock.flush()
-			# sleep(0.001)
+		#print(data)
+		sock.send(nodeid + "," + data)
 	except:
 		traceback.print_exc()
 		sock.close()

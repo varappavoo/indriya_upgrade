d90659bd0892f191f22b8e3cd92c6fecf83b7db6
cirlab <p.appavoo@gmail.com>
2017-08-24 15:49:22 +0800

    init commit

--- influxdb/zip_job_data.py
+++ influxdb/zip_job_data.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 import os
+from config import *
 
-time_from = 0
-time_to = 0
+job_id = "1"
+time_from = 1503460470000000000
+time_to = 1503461290502605824
 nodes_set = ['111','222']
 
+for nodeid in nodes_set:
+	command_get_data_for_job = "influx -database experiment -format csv -execute \"select * from test where time > " + str(time_from) + " and time < " + str(time_to) + " and nodeid='" + nodeid + "'\" -username indriya_admin -password 'in@fluxis!50Xfaster' > data_jobid_" + job_id + "_" + nodeid + ".csv"
+	print(os.system(command_get_data_for_job))
 
-command_get_data_for_job = "influx -database experiment -format csv -execute 'select * from test where time > 1503460470000000000 and time < 1503461290502605824 and nodeid='111'' -username indriya_admin -password 'in@fluxis!50Xfaster' > data_jobid_1.csv"
-command_zip_data_for_job = "zip data_jobid_1.zip data_jobid_1.csv"
-print(os.system(command_get_data_for_job))
+command_zip_data_for_job = "zip data_jobid_1.zip data_jobid_*.csv"
 print(os.system(command_zip_data_for_job))

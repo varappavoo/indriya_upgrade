f75b0843dd1babfafa6967cd42188003633d1457
varappavoo <pappavoo@comp.nus.edu.sg>
2017-08-24 19:52:20 +0800

    sending a copy of data as udp packet

--- influxdb/get_data_from_nodes.py
+++ influxdb/get_data_from_nodes.py
@@ -30,7 +30,7 @@ def check_nodes_status_from_db(manager_proxy_nodes_status):
 	while(True):
 		# list_of_nodes = {}
 		print("1")
-		nodes_status_file = open("nodes_db.csv",'r')
+		nodes_status_file = open("db_nodes.csv",'r')
 		line = nodes_status_file.readline() # header
 		line = nodes_status_file.readline()
 		print("2")
@@ -81,6 +81,16 @@ def start_collection_from(nodeid, manager_proxy_nodes_status):
 	sock_server.setsockopt(socket.IPPROTO_TCP, socket.TCP_QUICKACK, 1)
 	sock_server.connect((server_ip, server_port))
 
+	sock_rt_stream_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+	
+	# TCP_IP = '0.0.0.0'
+	# TCP_PORT = 9000 + int(nodeid)
+	# sock_dup = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	# sock_dup.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	# sock_dup.bind((TCP_IP, TCP_PORT))
+	# sock_dup.listen(100)
+	# (client_sock_dub, (ip,port)) = tcpServer.accept()
+
 	last_dangling_chunk = ""
 
 	count=0
@@ -94,6 +104,7 @@ def start_collection_from(nodeid, manager_proxy_nodes_status):
 				data_received = sock_node.recv(4096)
 				if not data_received: break
 				# data_received = data_received
+
 				data_received_split = data_received.decode('utf-8','ignore').split('\n')
 				# data_received_split = data_received.split('\n')
 				for i in range(len(data_received_split) - 1): # last chunk is likely to be incomplete
@@ -108,6 +119,9 @@ def start_collection_from(nodeid, manager_proxy_nodes_status):
 					data_string = json.dumps(json_data)
 					print(data_string)
 					sock_server.send(str.encode(data_string,'utf-8') + str.encode("\n")) # encode to from str to byte
+
+					sock_rt_stream_udp.sendto(str.encode(data_string,'utf-8') + str.encode("\n"), ('localhost', UDP_PORT+int(nodeid)))
+					print(UDP_PORT+int(nodeid))
 				last_dangling_chunk = data_received_split[-1]
 			except:
 				print(traceback.print_exc())

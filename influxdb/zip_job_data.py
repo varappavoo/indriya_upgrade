3ac0e44e3393c2a65552c2a19168c54f68baa212
cirlab <p.appavoo@gmail.com>
2017-08-24 16:08:21 +0800

    minor changes

--- influxdb/zip_job_data.py
+++ influxdb/zip_job_data.py
@@ -6,11 +6,12 @@ job_id = "1"
 time_from = 1503460470000000000
 time_to = 1503461290502605824
 nodes_set = ['111','222']
+directory = 'packed_data/'
 
 for nodeid in nodes_set:
-	command_get_data_for_job = "influx -database " + dbname + " -format csv -execute \"select * from " + table + " where time > " + str(time_from) + " and time < " + str(time_to) + " and nodeid='" + nodeid + "'\" -username '" + dbuser 
-	+ "' -password '" + dbuser_password + "' > data_jobid_" + job_id + "_" + nodeid + ".csv"
+	command_get_data_for_job = "influx -database " + dbname + " -format csv -execute \"select * from " + table + " where time > " + str(time_from) + " and time < " + str(time_to) + " and nodeid='" + nodeid + "'\" -username '" + dbuser + \
+	"' -password '" + dbuser_password + "' > " + directory +  "data_jobid_" + job_id + "_" + nodeid + ".csv"
 	print(os.system(command_get_data_for_job))
 
-command_zip_data_for_job = "zip data_jobid_1.zip data_jobid_*.csv"
+command_zip_data_for_job = "zip " + directory + "data_jobid_1.zip " + directory + "data_jobid_*.csv"
 print(os.system(command_zip_data_for_job))

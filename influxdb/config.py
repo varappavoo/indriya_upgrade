bb6112739295167d97fd95072df6fe96bbed533c
varappavoo <pappavoo@comp.nus.edu.sg>
2017-08-24 15:45:48 +0800

    init commit

---
+++ influxdb/config.py
@@ -0,0 +1,12 @@
+from secrets import *
+
+server_ip = "192.168.1.103"
+server_port = 9000
+
+dbname = 'experiment'
+dbuser = admin
+dbuser_password = admin_password
+table = 'test'
+host='localhost'
+port=8086
+db_batch_size=5000

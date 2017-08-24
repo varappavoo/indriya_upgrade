0ca9af21c7e11a520cceeabbed00ae683a7eb6e3
varappavoo <pappavoo@comp.nus.edu.sg>
2017-08-24 15:54:43 +0800

    minor changes

--- influxdb/zip_job_data.py
+++ influxdb/zip_job_data.py
@@ -8,7 +8,8 @@ time_to = 1503461290502605824
 nodes_set = ['111','222']
 
 for nodeid in nodes_set:
-	command_get_data_for_job = "influx -database experiment -format csv -execute \"select * from test where time > " + str(time_from) + " and time < " + str(time_to) + " and nodeid='" + nodeid + "'\" -username indriya_admin -password 'in@fluxis!50Xfaster' > data_jobid_" + job_id + "_" + nodeid + ".csv"
+	command_get_data_for_job = "influx -database " + dbname + " -format csv -execute \"select * from " + table + " where time > " + str(time_from) + " and time < " + str(time_to) + " and nodeid='" + nodeid + "'\" -username '" + dbuser 
+	+ "' -password '" + dbuser_password + "' > data_jobid_" + job_id + "_" + nodeid + ".csv"
 	print(os.system(command_get_data_for_job))
 
 command_zip_data_for_job = "zip data_jobid_1.zip data_jobid_*.csv"

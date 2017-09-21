06f70c30123b6e2c392cf440c32139e489c48d2c
varappavoo <pappavoo@comp.nus.edu.sg>
2017-09-21 16:15:21 +0800

    adding on_disconnect for mqtt

--- influxdb/aggregator.py
+++ influxdb/aggregator.py
@@ -38,12 +38,22 @@ tmp_time = start
 mqtt_broker="localhost"
 port=1883
 mqtt_client = mqtt.Client(client_id="indriya_server")
+mqtt_qos = 0 #QoS 0 at most once (fire and forget), 1 at least once (broker acknowledges),   2 exactly once hence most expensive
 
 def on_publish(client,userdata,result):
 	pass
 
+def on_disconnect(client, userdata, rc):
+    print("Disconnected from MQTT server: %s" % rc)
+    while rc != 0:
+        sleep(1)
+        print("Reconnecting...")
+        rc = client.reconnect()
+
+
 mqtt_client.on_publish = on_publish                       
 mqtt_client.connect(mqtt_broker,port) 
+mqtt_client.loop_start()
 
 def execute_request(start,json_body):
 	result =  client.write_points(json_body)#,time_precision='u')   
@@ -69,10 +79,11 @@ def savetodb_batching(json_data):
 	# 	print("invalid nodeid",data_split[0])
 	#     # print(traceback.print_exc())
 	for key in active_jobs.keys():
-		print("1",json_data['nodeid'],active_jobs[key])
+		#print("1",json_data['nodeid'],active_jobs[key])
 		if (json_data['nodeid'] in active_jobs[key]):
-			print("2",json_data['nodeid'],active_jobs[key])
-			mqtt_client.publish(key, json.dumps(json_data), True)
+			# print("2",key,json_data['nodeid'],active_jobs[key])
+			print("mqtt_client.publish",key, json.dumps(json_data), mqtt_qos)
+			print(mqtt_client.publish(key, json.dumps(json_data), mqtt_qos))
 
 	now = time()
 	if(now - tmp_time >= 10 or count==db_batch_size):

4b17c50e5785bb73e5aa9dc06217d63bbc62ef3b
varappavoo <p.appavoo@gmail.com>
2017-10-26 01:10:45 +0800

    burn results threads can be shared across several jobs

--- run_jobs.py
+++ run_jobs.py
@@ -23,8 +23,9 @@ with open('nodes_virt_id_phy_id.json') as json_data:
 	json_nodes_virt_id_phy_id = json.load(json_data)
 
 class ThreadBurnMote (threading.Thread):
-	def __init__(self, motetype, moteref, scp_command, ssh_burn_command):
+	def __init__(self, result_id, motetype, moteref, scp_command, ssh_burn_command):
 		threading.Thread.__init__(self)
+		self.result_id = result_id
 		self.motetype = motetype
 		self.moteref = moteref
 		self.scp_command = scp_command
@@ -34,7 +35,7 @@ class ThreadBurnMote (threading.Thread):
 		logger.warn("locking " + self.moteref)
 		tmp_mote_lock = fasteners.InterProcessLock('/tmp/tmp_mote_lock_' + self.moteref)
 		x = tmp_mote_lock.acquire(blocking=True)
-		execute_job(self.motetype, self.moteref, self.scp_command, self.ssh_burn_command)
+		execute_job(self.result_id, self.motetype, self.moteref, self.scp_command, self.ssh_burn_command)
 		tmp_mote_lock.release()
 		logger.warn("unlocking " + self.moteref)
 		print ("Exiting " + self.moteref)
@@ -53,17 +54,17 @@ def run_cmd(command, success_identifier):
 		logger.warning("FAILURE:" + command)
 		return False
 
-def execute_job(motetype, moteref,scp_command,ssh_burn_command):# scp and burn
+def execute_job(result_id, motetype, moteref,scp_command,ssh_burn_command):# scp and burn
 	# global burn_results
-	if burn_results['job_config'].get(motetype) == None:
-		burn_results['job_config'][motetype] = {}
-	burn_results['job_config'][motetype][moteref]={}
-	print(burn_results)
+	if burn_results[result_id]['job_config'].get(motetype) == None:
+		burn_results[result_id]['job_config'][motetype] = {}
+	burn_results[result_id]['job_config'][motetype][moteref]={}
+	print(burn_results[result_id])
 
 	count_burn_tries = 1
 
-	burn_results['job_config'][motetype][moteref]['scp'] = "1" if(run_cmd(scp_command, "Exit status 0")) else "0"
-	if burn_results['job_config'][motetype][moteref]['scp'] == "1":
+	burn_results[result_id]['job_config'][motetype][moteref]['scp'] = "1" if(run_cmd(scp_command, "Exit status 0")) else "0"
+	if burn_results[result_id]['job_config'][motetype][moteref]['scp'] == "1":
 		burn_done = "0"
 		while(count_burn_tries <= MAX_RETRIES_BURN and burn_done == "0"):
 			logger.warning(moteref + " - BURNING TRY:" + str(count_burn_tries))
@@ -71,15 +72,17 @@ def execute_job(motetype, moteref,scp_command,ssh_burn_command):# scp and burn
 			count_burn_tries = count_burn_tries + 1
 			if count_burn_tries > 1:
 				sleep(WAIT_BEFORE_RETRY)
-		burn_results['job_config'][motetype][moteref]['burn'] = burn_done
+		burn_results[result_id]['job_config'][motetype][moteref]['burn'] = burn_done
 	else:
-		burn_results['job_config'][motetype][moteref]['burn'] = "0"
+		burn_results[result_id]['job_config'][motetype][moteref]['burn'] = "0"
 		logger.warning("Not attempting to burn as SCP was unsuccessful: \n" + scp_command)
-	print(burn_results)
+	print(burn_results[result_id])
 
 
 
 def schedule_job(json_jobs_waiting):
+		result_id = json_jobs_waiting['result_id']
+		burn_results[result_id] = {}
 		# with open('jobs_waiting.json') as json_data:
 		# global burn_results
 		# json_jobs_waiting = json.load(json_data)
@@ -88,11 +91,11 @@ def schedule_job(json_jobs_waiting):
 		num_threads = threading.activeCount()
 		# num_threads_new = 0
 		print(json_jobs_waiting)
-		burn_results['job_config']={}
+		burn_results[result_id]['job_config']={}
 		all_threads = []
 		for job in json_jobs_waiting['job_config']:
 			print(job)
-			burn_results['result_id']=json_jobs_waiting['result_id']
+			burn_results[result_id]['result_id']=json_jobs_waiting['result_id']
 
 			if(job['type'] == 'telosb'):
 				for mote in job['mote_list']:
@@ -120,7 +123,9 @@ def schedule_job(json_jobs_waiting):
 		# 	print("run_jobs:schedule_job:threading.activeCount",threading.activeCount())
 		# 	sleep(1)
 
-		print(burn_results)
-		logger(str(json_jobs_waiting['result_id']) + str(burn_results))
-		return burn_results
+		print(burn_results[result_id])
+		logger(str(json_jobs_waiting['result_id']) + str(burn_results[result_id]))
+		results = burn_results[result_id]+
+		burn_results.pop(result_id, None)
+		return results
 

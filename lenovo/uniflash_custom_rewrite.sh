14c5bfaa60a7ed70c0d3ce8b9455b5fa0d2b3dd5
varappavoo <p.appavoo@gmail.com>
2018-02-19 16:12:45 +0800

    flashing on 64bit OS

---
+++ lenovo/uniflash_custom_rewrite.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# 64bit OS
+flash_ccxml_file=$1
+program=$2
+other_params=" -targetOp reset restart run"
+command="/home/cirlab/flash_sensortag_linux_64/dslite.sh -v -c $flash_ccxml_file -f $program"
+echo $command
+$command
